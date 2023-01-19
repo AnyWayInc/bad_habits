59a51f8dc72eabcd69c3c0593ae92e5a901c2987
AnyWayInc <sasaev20011@gmail.com>
2023-01-19 17:15:18 +0300

    lesson3

--- js/app.js
+++ js/app.js
@@ -1,1 +1,61 @@
-console.log('err');
+// 1 упражнение
+/* Реализацтя первого упражнения
+const moneyInTime = 80;
+const timeInDay = 5;
+const timeInWeek = timeInDay * 5;
+const dayInWeek = 5;
+const timeOnWork = 40;
+const dayBeforeVacation = 11;
+const freeTime = timeInDay * dayBeforeVacation;
+
+console.log(timeOnWork>=freeTime? "No" : onclusionMoney = timeOnWork * moneyInTime+ "$");
+
+if(timeOnWork>=freeTime){
+    console.log("No");
+}else{
+    console.log("Yes");
+    conclusionMoney = timeOnWork * moneyInTime;
+    console.log("Я попрошу : ", conclusionMoney + "$");
+}
+*/
+
+//2 упражнение
+/*
+const deposit = 12000;
+const procent = 0.07;
+const priceHouse = 13500; 
+const result = deposit * (1 + procent / 12) ** 24;
+
+if(result>priceHouse){
+    const summAfterBuy = result - priceHouse;
+    console.log("Вася сможет купить дом, и у него останется:", summAfterBuy + "$");
+}else{
+    console.log('Вася не сможет купить дом, так как не хватает денег')
+}
+*/
+
+//3 упражнение 
+/*
+const num = 7 
+let userNum;
+const slog = prompt("Выберите + или -");
+
+if(slog==='+'){
+    userNum = prompt(`Сколько будет ${num} + 15`);
+}else if(slog==='-'){
+    userNum = prompt(`Сколько будет ${num} - 15`);
+}else{
+    alert('Ты чет не то ввел!');
+}
+
+
+if(Number(userNum) === 22){
+    console.log('Успех');
+}else if (Number(userNum) === -8){
+    console.log('Успех');
+}else if (userNum === 'Я не робот'){
+    console.log('Успех');
+}else {
+    console.log('Вы робот!');
+}
+*/
