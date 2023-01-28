6f69bd8b605e1eb11b775990665205c076421bac
AnyWayInc <sasaev20011@gmail.com>
2023-01-28 23:16:02 +0300

    Работа со строками и создание информационной базы

--- js/app.js
+++ js/app.js
@@ -264,3 +264,156 @@ console.log(positivePrices);
 
 
 //12 упражнение
+
+/*
+const arr = [1 , 4, 4, 10];
+
+const average = arr.reduce((acc,value,i)=>{
+    if(i != arr.length-1){
+        return acc + value;
+    }else{
+        return (acc + value) / arr.length;
+    }
+},0);
+
+console.log(average);
+*/
+
+
+//13 упражнение
+
+/*
+const arr = [2, 4, 4, 10, 20];
+
+function some(arr,value){
+    const newArr = arr.find(el=>el === value);
+    return (newArr === undefined) ? false: true;
+}
+
+console.log(some(arr,4));
+*/
+
+
+//14 упражнение 
+
+//Работа с sort
+/*
+    const operations = [100,-11,-123,213,1252];
+    //<0 то будет выводить порядок a,b
+    //>0 то будет выводить порядок b,a
+
+    operations.sort((a, b)=> a-b); //сортировка от меньшего к большему
+    console.log(operations);
+*/
+
+//Быстрое создание массивов
+/*
+    const arr = [1,2,3,4,5];
+
+    console.log(new Array(1,2,3,4,5));
+    const arr2 = new Array(5);//быстрое создание пустого массива из 5 элементов перед тем как с ним работать нужно его заполнить
+    arr.fill(1);//Весь массив заполняется 1 arr.fill(1,0,3) с 0 позиции до 3 заполняется 1
+    const arr3 = Array.from({length:5},(cur,i)=> i+1); Также как fill запонлняем массив по i+1 [1,2,3,4,5]; 
+*/
+
+//Раздел работы со строками
+
+/*
+    const userName = 'Вася Пупкин';
+    console.log(userName[0]);//Выведет 'В'
+    console.log(userName.charAt(2));//Выведет 'c' выводит символ 
+    //Эмоджи занимают 2 или больше символа так что может некорректно работать
+    console.log(userName.length);//Длина строки
+    console.log(userName.indexOf('П'));//Выведет индекс буквы 'П', если буквы 2 то выведет первое встречающееся , если ввести 2 буквы то выведет индекс первого попавшегося идентичного сочетания
+    console.log(userName.lastIndexOf('П'));//Выведет индекс буквы 'П', если буквы 2 то выведет последнее встречающееся
+    console.log(userName.includes('уп'));//Проверить наличие буквы/сочетания букв
+    console.log(userName.slice(4));//Обрезает все что после 4 элемента и выводит в отдельный массив
+*/
+
+//Вывести Имя и Фамилию в отдельные переменные 
+/*
+    const userName = 'Вася aka Terminator Пупкин';
+
+    const firstName = userName.slice(0,4);
+    const secondName = userName.slice(userName.length-6);
+    console.log(firstName, secondName);
+*/
+
+
+//15 упражнение
+
+
+//Преобразование строки 
+/*
+    const str = 'Вася Пупкина';
+    console.log(str.includes('a'));// Выводит наличие 'а'
+    console.log(str.startsWith('В'));// Начинается с 'В' тогда выводит true 
+    console.log(str.endsWith('н'));//Заканчивается 'н' то выводит true
+    console.log(new String('Вася Пупкин').includes('а'));// Создание нового объекта типа строка и выводит true если в ней есть 'а'
+
+    console.log(str.toLowerCase());//Все с маленькой
+    console.log(str.toUpperCase());//Все с большой 
+    console.log(str.replace('В','Ф'));//Заменяет первую'В' на 'Ф' возвращая новую строку 
+    console.log(str.replaceAll('а','и'));//Заменяет все 'а' на 'и' возвращая новую строку 
+    console.log(str.replace(/а/g,'и'))//Заменяет все 'а' на 'и' возвращая новую строку 
+
+    const str2 = 'Вася Пупкина  \n';
+    console.log(str.trim());//Очищает все пробелы в строке есть trimStart и trimEnd которые очищают пробелы в начале и конце соотвественно
+    
+*/
+
+//Проверить являются ли это номером телефона РФ
+
+
+/*Верные*/
+// const num1 = '89103235356';
+// const num2 = '89103235356';
+// const num3 = '+7(910)3235356';
+// const num4 = '+7(910)323-53-56';
+// const num5 = '  +7(910)323-53-56  ';
+
+// /*Не верные*/
+
+// const num1Error = '89103235';
+// const num2Error = '+7d9103235';
+// const num3Error = '9+79103999235';
+// const num4Error = '89103g21235';
+
+
+//const currentNumber = String(prompt('Введите номер телефона'));
+
+// function checkNumber (currentNumber){
+
+//     currentNumber = currentNumber.trim();
+//     currentNumber = currentNumber.replace('+7','8');
+
+//     if(!currentNumber.startsWith(8)){
+//         return false;
+//     }
+
+//     currentNumber = currentNumber.replaceAll('(','');
+//     currentNumber = currentNumber.replaceAll(')','');
+//     currentNumber = currentNumber.replaceAll('+','');
+//     currentNumber = currentNumber.replaceAll('-','');
+
+
+//     if(currentNumber.length == 11 && !isNaN(Number(currentNumber)) ){ //Проверяет является ли то что внутри строки Not a Number
+//         return true ;
+//     }else{
+//         return false;
+//     }
+// }
+
+// console.log(checkNumber(num1));
+// console.log(checkNumber(num2));
+// console.log(checkNumber(num3));
+// console.log(checkNumber(num4));
+// console.log(checkNumber(num5));
+
+// console.log(checkNumber(num1Error));
+// console.log(checkNumber(num2Error));
+// console.log(checkNumber(num3Error));
+// console.log(checkNumber(num4Error));
+
+
+//16 упражнение
